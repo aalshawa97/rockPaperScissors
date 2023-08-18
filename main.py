f888c22672292811d93a3ad09a484d1bf2f6f359
Abdullah Mutaz Alshawa <abdullahalshawa@gmail.com>
2023-08-18 09:56:41 -0700

    Update main.py
    Alternate version

--- main.py
+++ main.py
@@ -1,56 +1,39 @@
-# Abdullah Alshawa
 import random
 
-print('Welcome')
-# 0 is rock, 1 is paper, and 2 is scissors
-r = random.randint(0, 2)
-prmpt = input('Rock, paper, or scissors?')
-if prmpt == 'Rock' and r == 0:
-    print('You choose Rock')
-    print('Computer choose Rock')
-    print('Draw')
-    exit()
-if prmpt == 'Rock' and r == 1:
-    print('You choose Rock')
-    print('Computer choose paper')
-    print('Lost')
-    exit()
-if prmpt == 'Rock' and r == 2:
-    print('You choose Rock')
-    print('Computer choose scissors')
-    print('Won')
-    exit()
+def get_choices():
+    player_choice = input('Could you please choose rock, paper, or scissors?').capitalize()
+    options = ["Rock", "Paper", "Scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
 
-if prmpt == 'Paper' and r == 0:
-    print('You choose Paper')
-    print('Computer choose Rock')
-    print('Won')
-    exit()
-if prmpt == 'Paper' and r == 1:
-    print('You choose Paper')
-    print('Computer choose paper')
-    print('Draw')
-    exit()
-if prmpt == 'Paper' and r == 2:
-    print('You choose Paper')
-    print('Computer choose scissors')
-    print('Lost')
-    exit()
+def check_win(player, computer):
+    if player == computer:
+        print("The computer choose: " + computer)
+        return "It's a tie!"
+    if player == 'Rock' and computer == 'Scissors':
+        print("The computer choose: " + computer)
+        return "Player wins!"
+    if player == 'Rock' and computer == 'Paper':
+        print("The computer choose: " + computer)
+        return "Computer wins!"
+    if player == 'Paper' and computer == 'Rock':
+        print("The computer choose: " + computer)
+        return "Player wins!"
+    if player == 'Paper' and computer == 'Scissors':
+        print("The computer choose: " + computer)
+        return "Computer wins!"
+    if player == 'Scissors' and computer == 'Rock':
+        print("The computer choose: " + computer)
+        return "Computer wins!"
+    if player == 'Scissors' and computer == 'Paper':
+        print("The computer choose: " + computer)
+        return "Player wins!"
 
-if prmpt == 'Scissors' and r == 0:
-    print('You choose Scissors')
-    print('Computer choose Rock')
-    print('Lost')
-    exit()
-if prmpt == 'Scissors' and r == 1:
-    print('You choose Scissors')
-    print('Computer choose paper')
-    print('Won')
-    exit()
-if prmpt == 'Scissors' and r == 2:
-    print('You choose Scissors')
-    print('Computer choose scissors')
-    print('Draw')
-    exit()
+choices = get_choices()
+player_choice = choices["player"]
+computer_choice = choices["computer"]
 
-print('Goodbye')
+result = check_win(player_choice, computer_choice)
+print(result)
+
